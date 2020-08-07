1d3564e0e0916949dc4a26e9e244d459b2f4460d
nevraka <nevrakaya@gmail.com>
2020-08-07 19:29:22 +0200

    changed main result page

--- src/components/MainResult.js
+++ src/components/MainResult.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import { List } from 'antd';
+import { List, Divider } from 'antd';
 import './App.css';
+import moment from 'moment';
 
 const MainResult = ({ jobs }) => {
   return (
     <div className="all-results">
       All Jobs
-      <List
-        className="main-job-results"
-        itemLayout="horizontal"
-        dataSource={jobs}
-        renderItem={(item) => (
-          <List.Item>
-            <List.Item.Meta
-              title={<a href="https://ant.design">{item.title}</a>}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            />
-          </List.Item>
-        )}
-      />
+      <Divider />
+      <div>
+        <List
+          className="main-job-results"
+          itemLayout="horizontal"
+          dataSource={jobs}
+          renderItem={(job) => (
+            <List.Item key={job.id}>
+              <List.Item.Meta
+                description={
+                  <div className="description">
+                    <div className="company-title">
+                      <a>{job.title}</a>
+                      <div className="company-type">
+                        <a>{job.company}</a> &nbsp;-<div>{job.type}</div>
+                      </div>
+                    </div>
+                    <div className="result-right">
+                      <div>{job.location}</div>
+                      <div>{moment(job.created_at).fromNow()}</div>
+                    </div>
+                  </div>
+                }
+              />
+            </List.Item>
+          )}
+        />
+      </div>
     </div>
   );
 };
-
 export default MainResult;
