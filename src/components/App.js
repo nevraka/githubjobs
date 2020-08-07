a4949cd0a5317843dc345c86d055e8bbd2352acd
nevraka <nevrakaya@gmail.com>
2020-08-07 19:30:47 +0200

    some changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Table, Layout, Divider } from 'antd';
-import { GithubOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Layout } from 'antd';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import githubJobs from '../api/githubJobs';
 import SearchBox from './SearchBox';
 import MainResult from './MainResult';
+import JobDetails from './JobDetails';
+import AppHeader from './AppHeader';
+import AppFooter from './AppFooter';
 import './App.css';
 
-const { Column } = Table;
-const { Header, Footer, Content } = Layout;
+const { Content } = Layout;
 
 const App = () => {
   const [desc, setDesc] = useState('');
@@ -22,66 +24,36 @@ const App = () => {
     setJobs(result.data);
   };
 
-  useEffect(() => {
-    onSearch();
-  }, []);
-
   return (
-    <Layout>
-      <Header id="header">
-        GitHub Jobs
-        <div className="all-header-button">
-          <Button type="link" className="header-button">
-            All jobs
-          </Button>
-          <Divider type="vertical" className="divider" />
-          <Button type="link" className="header-button">
-            Post a job
-          </Button>
-          <Divider type="vertical" className="divider" />
-          <Button type="link" className="header-button">
-            How it works
-          </Button>
-        </div>
-      </Header>
-      <div>
-        <SearchBox
-          className="search-div"
-          setFullTime={setFullTime}
-          onSearch={onSearch}
-          desc={desc}
-          setDesc={setDesc}
-          loc={loc}
-          setLoc={setLoc}
-        />
-      </div>
-      <Content>
-        <MainResult jobs={jobs} />
-      </Content>
-      <Footer>
-        <div>
-          <Button type="link" className="footer-button">
-            The GitHub Blog
-          </Button>
-          <Button type="link" className="footer-button">
-            Contact
-          </Button>
-          <Button type="link" className="footer-button">
-            Privacy
-          </Button>
-          <Button type="link" className="footer-button">
-            Terms
-          </Button>
-          <Button type="link" className="footer-button">
-            API
-          </Button>
-          <GithubOutlined className="github" />
-          <span className="reserved">
-            © 2020 GitHub Inc. All rights reserved.
-          </span>
-        </div>
-      </Footer>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <AppHeader />
+        <Switch>
+          <Route path="/position/:id">
+            <JobDetails />
+          </Route>
+          <Route path="/">
+            <div>
+              <SearchBox
+                className="search-div"
+                setFullTime={setFullTime}
+                onSearch={onSearch}
+                desc={desc}
+                setDesc={setDesc}
+                loc={loc}
+                setLoc={setLoc}
+                jobs={jobs}
+                setJobs={setJobs}
+              />
+            </div>
+            <Content>
+              <MainResult jobs={jobs} />
+            </Content>
+          </Route>
+        </Switch>
+        <AppFooter />
+      </Layout>
+    </BrowserRouter>
   );
 };
 
