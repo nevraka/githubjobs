266c72b007e0d947ac1193c9aca63a19ba2c282a
nevraka <nevrakaya@gmail.com>
2021-06-07 23:41:20 +0200

    title logic

--- pages/index.js
+++ pages/index.js
@@ -5,52 +5,54 @@ import 'github-fork-ribbon-css/gh-fork-ribbon.css';
 import axios from 'axios';
 
 const MainPage = () => {
-  const [desc, setDesc] = useState('');
-  const [loc, setLoc] = useState('');
-  const [jobs, setJobs] = useState([]);
-  const [fullTime, setFullTime] = useState(false);
+    const [desc, setDesc] = useState('');
+    const [loc, setLoc] = useState('');
+    const [jobs, setJobs] = useState([]);
+    const [fullTime, setFullTime] = useState(false);
+    const [title, setTitle] = useState('Featured Jobs');
 
-  useEffect(() => {
-    const loadFeatured = async () => {
-      const result = await axios.get(`/api/positions?search=github`);
-      console.log(result);
-      setJobs(result.data.slice(0, 3));
+    useEffect(() => {
+        const loadFeatured = async () => {
+            const result = await axios.get(`/api/positions?search=github`);
+            console.log(result);
+            setJobs(result.data.slice(0, 3));
+        };
+        loadFeatured();
+    }, []);
+
+    const onSearch = async () => {
+        const result = await axios.get(
+            `/api/positions?search=${desc}&location=${loc}&full_time=${fullTime}`
+        );
+        console.log(result);
+        setJobs(result.data);
+        setTitle('Search Results');
     };
-    loadFeatured();
-  }, []);
 
-  const onSearch = async () => {
-    const result = await axios.get(
-      `/api/positions?search=${desc}&location=${loc}&full_time=${fullTime}`
+    return (
+        <>
+            <SearchBox
+                className="search-div"
+                setFullTime={setFullTime}
+                onSearch={onSearch}
+                desc={desc}
+                setDesc={setDesc}
+                loc={loc}
+                setLoc={setLoc}
+                jobs={jobs}
+                setJobs={setJobs}
+            />
+            <Result jobs={jobs} title={title} />
+            <a
+                class="github-fork-ribbon right-bottom fixed"
+                href="https://github.com/nevraka/githubjobs"
+                data-ribbon="Fork me on GitHub"
+                title="Fork me on GitHub"
+            >
+                Fork me on GitHub
+            </a>
+        </>
     );
-    console.log(result);
-    setJobs(result.data);
-  };
-
-  return (
-    <>
-      <SearchBox
-        className="search-div"
-        setFullTime={setFullTime}
-        onSearch={onSearch}
-        desc={desc}
-        setDesc={setDesc}
-        loc={loc}
-        setLoc={setLoc}
-        jobs={jobs}
-        setJobs={setJobs}
-      />
-      <Result jobs={jobs} title="All Jobs" />
-      <a
-        class="github-fork-ribbon right-bottom fixed"
-        href="https://github.com/nevraka/githubjobs"
-        data-ribbon="Fork me on GitHub"
-        title="Fork me on GitHub"
-      >
-        Fork me on GitHub
-      </a>
-    </>
-  );
 };
 
 export default MainPage;
