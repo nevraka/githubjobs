7d9ed1f7d5f2811e14c3f6f6956933dece4a9845
nevraka <nevrakaya@gmail.com>
2021-06-08 16:25:10 +0200

    delete onsearch

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,36 +4,40 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 const { Header } = Layout;
 
-const AppHeader = ({ onSearch }) => {
-  const router = useRouter();
+const AppHeader = () => {
+    const router = useRouter();
 
-  return (
-    <Header
-      id="header"
-      style={{ padding: '10px 15px 0px', height: 'auto', minHeight: '64px' }}
-    >
-      <Row justify="space-between">
-        <Col>
-          <Link href="/" className="header-brand">
-            GitHub Jobs
-          </Link>
-        </Col>
-        <Col className="all-header-button">
-          <Link href="/" className="header-button">
-            All jobs
-          </Link>
-          <Divider type="vertical" className="divider" />
-          <Link href="/post" className="header-button">
-            Post a job
-          </Link>
-          <Divider type="vertical" className="divider" />
-          <Link href="/faq" className="header-button">
-            How it works
-          </Link>
-        </Col>
-      </Row>
-    </Header>
-  );
+    return (
+        <Header
+            id="header"
+            style={{
+                padding: '10px 15px 0px',
+                height: 'auto',
+                minHeight: '64px',
+            }}
+        >
+            <Row justify="space-between">
+                <Col>
+                    <Link href="/" className="header-brand">
+                        GitHub Jobs
+                    </Link>
+                </Col>
+                <Col className="all-header-button">
+                    <Link href="/" className="header-button">
+                        All jobs
+                    </Link>
+                    <Divider type="vertical" className="divider" />
+                    <Link href="/post" className="header-button">
+                        Post a job
+                    </Link>
+                    <Divider type="vertical" className="divider" />
+                    <Link href="/faq" className="header-button">
+                        How it works
+                    </Link>
+                </Col>
+            </Row>
+        </Header>
+    );
 };
 
 export default AppHeader;
