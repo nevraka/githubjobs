15624b4c3371e759720ffe2697d52b934a7f71dc
nevraka <nevrakaya@gmail.com>
2021-06-09 23:20:17 +0200

    header link name change

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,43 +1,41 @@
 import React from 'react';
 import { Layout, Divider, Row, Col } from 'antd';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+
 const { Header } = Layout;
 
 const AppHeader = () => {
-    const router = useRouter();
-
-    return (
-        <Header
-            id="header"
-            style={{
-                padding: '10px 15px 0px',
-                height: 'auto',
-                minHeight: '64px',
-            }}
-        >
-            <Row justify="space-between">
-                <Col>
-                    <Link href="/" className="header-brand">
-                        GitHub Jobs
-                    </Link>
-                </Col>
-                <Col className="all-header-button">
-                    <Link href="/" className="header-button">
-                        All jobs
-                    </Link>
-                    <Divider type="vertical" className="divider" />
-                    <Link href="/post" className="header-button">
-                        Post a job
-                    </Link>
-                    <Divider type="vertical" className="divider" />
-                    <Link href="/faq" className="header-button">
-                        How it works
-                    </Link>
-                </Col>
-            </Row>
-        </Header>
-    );
+  return (
+    <Header
+      id="header"
+      style={{
+        padding: '10px 15px 0px',
+        height: 'auto',
+        minHeight: '64px',
+      }}
+    >
+      <Row justify="space-between">
+        <Col>
+          <Link href="/" className="header-brand">
+            GitHub Jobs
+          </Link>
+        </Col>
+        <Col className="all-header-button">
+          <Link href="/" className="header-button">
+            All jobs
+          </Link>
+          <Divider type="vertical" className="divider" />
+          <Link href="/post" className="header-button">
+            Post a job
+          </Link>
+          <Divider type="vertical" className="divider" />
+          <Link href="/how-it-works" className="header-button">
+            How it works
+          </Link>
+        </Col>
+      </Row>
+    </Header>
+  );
 };
 
 export default AppHeader;
--- src/components/AppHeader.test.js
+++ src/components/AppHeader.test.js
@@ -3,19 +3,19 @@ import { shallow } from 'enzyme';
 import AppHeader from './AppHeader';
 
 it('Test links', () => {
-    const wrapper = shallow(<AppHeader />);
-    const url = wrapper.find('Link').first().prop('href');
-    expect(url).toBe('/');
+  const wrapper = shallow(<AppHeader />);
+  const url = wrapper.find('Link').first().prop('href');
+  expect(url).toBe('/');
 
-    const wrapper1 = shallow(<AppHeader />);
-    const url1 = wrapper1.find('Link').at(1).prop('href');
-    expect(url1).toBe('/');
+  const wrapper1 = shallow(<AppHeader />);
+  const url1 = wrapper1.find('Link').at(1).prop('href');
+  expect(url1).toBe('/');
 
-    const wrapper2 = shallow(<AppHeader />);
-    const url2 = wrapper2.find('Link').at(2).prop('href');
-    expect(url2).toBe('/post');
+  const wrapper2 = shallow(<AppHeader />);
+  const url2 = wrapper2.find('Link').at(2).prop('href');
+  expect(url2).toBe('/post');
 
-    const wrapper3 = shallow(<AppHeader />);
-    const url3 = wrapper3.find('Link').at(3).prop('href');
-    expect(url3).toBe('/faq');
+  const wrapper3 = shallow(<AppHeader />);
+  const url3 = wrapper3.find('Link').at(3).prop('href');
+  expect(url3).toBe('/how-it-works');
 });
